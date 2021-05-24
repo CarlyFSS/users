9dc444079988c6fdd7d78c96e4875b69dae101d3
Carly Silva <carly.fs.silva@gmail.com>
2021-05-24 11:27:02 -0300

    feat: add develop branch

--- src/main.ts
+++ src/main.ts
@@ -28,8 +28,6 @@ async function bootstrap() {
   //   httpClient = await NestFactory.create(AppModule);
   // else httpClient = await NestFactory.create(AppModule);
 
-  // Test husky
-
   const httpClient = await NestFactory.create(AppModule);
 
   // Setup Swagger/Open Api
